e852b81af25c2045401ee66a302a5bc41969a0a8
Patricio Viccini <pato98115@gmail.com>
2020-07-01 19:54:49 -0300

    mensaje de bienvenida, botton y variable de nombre de usuario

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,20 @@
-const myHeading = document.querySelector('h1');
+let myHeading = document.querySelector('h1');
+let subHeading = document.querySelector('h2');
 let chimueImage = document.querySelector('img');
+let nameButton = document.querySelector('button');
+
+function setUserName() {
+    let userName = prompt('Me dices tu nombre, por favor?');
+    localStorage.setItem('name', userName);
+    subHeading.textContent = `Bienvenido a mi página ${userName}!`;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    subHeading.textContent = `Bienvenido a mi página ${storedName}!`;
+}
 
 chimueImage.onclick = () => {
     let imgSrc = chimueImage.getAttribute('src');
@@ -13,4 +28,8 @@ chimueImage.onclick = () => {
         chimueImage.setAttribute('src', 'images/chimue-1er-plano.jpeg');
         myHeading.textContent = 'Sentada arriba de mi mamá <3';
     }
+};
+
+nameButton.onclick = function () {
+    setUserName();
 };
