797a5043523046f2cf346c3a06388c16c35dd52b
Patricio Viccini <pato98115@gmail.com>
2020-07-01 18:07:31 -0300

    toggle entre dos fotos al hacer click en la imagen

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,16 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myHeading = document.querySelector('h1');
+let chimueImage = document.querySelector('img');
+
+chimueImage.onclick = () => {
+    let imgSrc = chimueImage.getAttribute('src');
+    if (imgSrc === 'images/chimue-1er-plano.jpeg') {
+        chimueImage.setAttribute(
+            'src',
+            'images/chimue-acostada-al-lado-del-futon.jpeg'
+        );
+        myHeading.textContent = 'Pues me acuesto';
+    } else {
+        chimueImage.setAttribute('src', 'images/chimue-1er-plano.jpeg');
+        myHeading.textContent = 'Sentada arriba de mi mamá <3';
+    }
+};
